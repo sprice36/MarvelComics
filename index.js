170acfddda5189a3ce87f8ee6bde5d51c0e7d6d7
Stephen <smjarr89@gmail.com>
2018-06-27 13:34:52 -0400

    added basic api function calling ability

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ dotenv.config();
 
 const express = require('express');
 const app = express();
+const static = express.static;
+const rp = require('request-promise');
+const apiURL = 'http://gateway.marvel.com/v1/public/';
+const apiKey = process.env.API_KEY;
+const publicKey = process.env.PUBLIC_KEY;
+const md5 = require('md5');
 
 const setupAuth = require('./auth');
 const ensureAuthenticated = require('./auth').ensureAuthenticated;
@@ -27,8 +33,66 @@ app.use(staticMiddleware);
 
 // setupAuth(app);
 
+// app.get('/:character', (req, res) = {
+
+// });
+
+app.get('/comics', (req, res) => {
+    // getMarvelResponse();
+    // }
+    let ts = new Date().getTime();
+    let hash = md5(ts + apiKey + publicKey);
+    let apiAuthenticationString = 'ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
+    let requestURL = apiURL + 'comics/14038?' + apiAuthenticationString;
+    console.log(requestURL);
+
+    rp(requestURL)
+        .then((data)=> {
+            let comics = JSON.parse(data);
+            let results = comics.data.results[0]
+            // console.log(comics);
+            res.render('comics', {
+                results
+            });
+        }).catch((error) => {
+            res.send(error);
+        }); 
+    }
+);
+
+// Request Url: http://gateway.marvel.com/v1/public/comics
+// Request Method: GET
+// Params: {
+//   "apikey": "your api key",
+//   "ts": "a timestamp",
+//   "hash": "your hash"
+// }
+// Headers: {
+//   Accept: */*
+// }
 
 //server initialization
 app.listen(process.env.PORT, () => {
     console.log(`Your server is running at http://localhost:${process.env.PORT}`);
 })
+
+function getMarvelResponse () {
+    let ts = new Date().getTime();
+    let hash = md5(ts + apiKey + publicKey);
+    let searchType = 'comics/3750s'
+    // let options = {
+    //     uri: apiURL,
+    //     apikey: publicKey,
+    //     hash: hash
+    // }
+    let testURL = apiURL + 'comics/37502' + '?ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
+    console.log(testURL);
+    rp(testURL)
+        .then((data) => {
+            console.log(data);
+            // res.send(data);
+        })
+        .catch((error) => {
+            console.log(error.message);
+        })
+}
