343bdd6c78afa2ca9e8022c02102755da3585e35
Stephen <smjarr89@gmail.com>
2018-06-28 11:14:53 -0400

    put all functions in api function page

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const apiURL = 'http://gateway.marvel.com/v1/public/';
 const apiKey = process.env.API_KEY;
 const publicKey = process.env.PUBLIC_KEY;
 const md5 = require('md5');
-const apiFunctions = require('./searchapi');
+const {
+    searchComicsByCharID,
+    searchComicsByCharName,
+    searchAllComics,
+    searchAllCharacters
+} = require('./searchapi');
 
 const setupAuth = require('./auth');
 const ensureAuthenticated = require('./auth').ensureAuthenticated;
@@ -34,87 +39,56 @@ app.use(staticMiddleware);
 
 // setupAuth(app);
 
+//search comics associated with certain character
 app.get('/characters/:id', (req, res) => {
-    let comicData = apiFunctions.searchComicsByCharName(req.params.id)
+    let comicData = searchComicsByCharID(req.params.id);
     comicData
         .then((comicData) => {
             if (comicData === 'character not found') {
                 res.send('character not found')
             } else {
-                console.log(comicData);
                 res.render('singleCharacter', {comicData});
             }
         })
 });
 
+//search all comics
 app.get('/comics', (req, res) => {
-    let ts = new Date().getTime();
-    let hash = md5(ts + apiKey + publicKey);
-    let apiAuthenticationString = '&ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash;
-    let requestURL = apiURL + 'comics/14038?' + apiAuthenticationString;
-    // console.log(requestURL);
+    let allComics = searchAllComics()
+    allComics
+        .then((allComics) => {
+            if (allComics === 'there was an error') {
+                res.send('ERROR')
+            } else {
+                // console.log(allComics);
+                res.render('library', {
+                    allComics
+                });
+            }
+        });
+});
 
-    rp(requestURL)
-        .then((data)=> {
-            let comics = JSON.parse(data);
-            let results = comics.data.results[0];
-            // console.log(comics);
-            res.render('comics', {
-                results
-            });
-        }).catch((error) => {
-            res.send(error);
-        }); 
-    }
-);
-
-app.get('/library', (req, res) => {
-  
-    let ts = new Date().getTime();
-    let hash = md5(ts + apiKey + publicKey); 
-    let apiAuthenticationString = 'ts=' + ts +  '&apikey=' + publicKey + '&hash=' + hash; 
-    let requestURL = apiURL + 'comics?'  + 'limit=20&' + apiAuthenticationString;
-    console.log(requestURL);
-
-    rp(requestURL)
-        .then((data)=> {
-            let comics = JSON.parse(data);
-            let results = comics.data.results;
-              // console.log(results);
-            res.render('library', {
-                results 
-            });
-        }).catch((error) => {
-            res.send(error);
-        }); 
-    }  
-);
-
+//search all characters
 app.get('/characters', (req, res) => {
-  
-    let ts = new Date().getTime();
-    let hash = md5(ts + apiKey + publicKey); 
-    let apiAuthenticationString = 'ts=' + ts +  '&apikey=' + publicKey + '&hash=' + hash; 
-    let requestURL = apiURL + 'characters?'  + 'limit=20&' + apiAuthenticationString;
-    console.log(requestURL);
-
-    rp(requestURL)
-        .then((data)=> {
-            let comics = JSON.parse(data);
-            let results = comics.data.results;
-              // console.log(results);
-            res.render('characters', {
-                results 
-            });
-        }).catch((error) => {
-            res.send(error);
-        }); 
+    let allCharacters = searchAllCharacters()
+    allCharacters
+        .then((allCharacters) => {
+            if (allCharacters === 'there was an error') {
+                res.send('ERROR')
+            } else {
+                // console.log(allComics);
+                res.render('characters', {
+                    allCharacters
+                });
+            }
+        });
     }  
 );
 
-
-
-
+//server initialization
+app.listen(process.env.PORT, () => {
+    console.log(`Your server is running at http://localhost:${process.env.PORT}`);
+})
 
 // Request Url: http://gateway.marvel.com/v1/public/comics
 // Request Method: GET
@@ -126,9 +100,3 @@ app.get('/characters', (req, res) => {
 // Headers: {
 //   Accept: */*
 // }
-
-//server initialization
-app.listen(process.env.PORT, () => {
-    console.log(`Your server is running at http://localhost:${process.env.PORT}`);
-})
-
