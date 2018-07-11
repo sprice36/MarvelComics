7559c6e12110843f87e1e68032f850a0e6517463
Stephen <smjarr89@gmail.com>
2018-07-11 11:46:39 -0400

    updated index.js event listener

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,10 +2,6 @@ var navBarToggler = document.querySelector('.navbar-toggler');
 var navBarUL = document.querySelector('[data-navbar-ul');
 var navBarDiv = document.querySelector('[data-navbar');
 
-navBarToggler.addEventListener('click', () => {
-    navBarUL.classList.toggle('float-right');
-    navBarDiv.classList.toggle('collapse');
-})
 
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function () {
@@ -14,6 +10,10 @@ if ('addEventListener' in document) {
 }
 
 document.onclick = function () {
+    navBarToggler.addEventListener('click', () => {
+    navBarUL.classList.toggle('float-right');
+    navBarDiv.classList.toggle('collapse');
+    })
 }
 
-div.onclick = function () {}
+navBarToggler.onclick = function () {}
