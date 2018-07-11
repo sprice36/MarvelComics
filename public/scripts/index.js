330bb8636c0245f6f4923a8a74fe46b4f95055e1
Stephen <smjarr89@gmail.com>
2018-07-11 09:51:27 -0400

    added fastclick js library

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,3 +6,9 @@ navBarToggler.addEventListener('click', () => {
     navBarUL.classList.toggle('float-right');
     navBarDiv.classList.toggle('collapse');
 })
+
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function () {
+        FastClick.attach(document.body);
+    }, false);
+}
